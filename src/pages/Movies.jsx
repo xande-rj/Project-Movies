c3469f1e5c1076379f7bde7d29285df7aa06fe90
Alexandre <alexandresargitario@gmail.com>
2023-08-09 15:57:05 -0300

    End of project

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import { useEffect,useState } from "react"
+import { useParams } from "react-router-dom"
+import{
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from 'react-icons/bs'
 
-export default function Movies() {
+import MovieCard from '../Components/MovieCard'
+
+import './MovieGrid.css'
+import './Movie.css'
+
+const movieURl = import.meta.env.VITE_API
+
+const apiKey = import.meta.env.VITE_API_KEY
+
+export default function Movie() {
+      const {id} = useParams()
+
+      const [movie,setMovie] = useState(null)
+
+      const getMovie = async (url)=>{
+        const res = await fetch(url)
+        const data = await res.json()
+        
+        setMovie(data)
+      }
+
+      const formatCurrency = (number)=>{
+        return number.toLocaleString("en-US",{
+          style: "currency",
+          currency: "USD",
+        })
+      }
+
+      useEffect(()=>{
+        const movieUrl = `${movieURl}${id}?${apiKey}`
+    
+        getMovie(movieUrl)
+      },[])
+
+
   return (
-    <div>Movies</div>
+    <div className="movie-page">
+      {movie && (
+      <>
+      <MovieCard movie={movie} shortLink={false}/>
+      <p className="tagline">{movie.tagline}</p>
+      <div className="info">
+        <h3> <BsWallet2/> Budget : </h3>
+        <p>{formatCurrency(movie.budget)}</p>
+      </div>
+      <div className="info">
+        <h3> <BsGraphUp/> Invoicing : </h3>
+        <p>{formatCurrency(movie.revenue)}</p>
+      </div>
+      <div className="info">
+        <h3> <BsHourglassSplit/> Duration : </h3>
+        <p>{movie.runtime} Minutes</p>
+      </div>
+      <div className="info description">
+        <h3> <BsFillFileEarmarkTextFill/> Description : </h3>
+        <p>{movie.overview}</p>
+      </div>
+      </>
+      )}
+    </div>
   )
 }
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,6 +1,44 @@
+import { useEffect,useState } from "react"
+import { useSearchParams } from "react-router-dom"
 
-export default function search() {
+
+import MovieCard from '../Components/MovieCard'
+
+import './MovieGrid.css'
+
+const searchUrl = import.meta.env.VITE_SEARCH
+
+const apiKey = import.meta.env.VITE_API_KEY
+
+export default function Search() {
+      const [searchParams] = useSearchParams()
+
+      const [movies,setMovies] = useState([])
+
+      const query = searchParams.get('q')
+
+      const getSearchMovies = async (url)=>{
+        const res = await fetch(url)
+        const data = await res.json()
+        
+        setMovies(data.results)
+      }
+      useEffect(()=>{
+        const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`
+    
+        getSearchMovies(searchWithQueryUrl)
+      },[])
+
+
   return (
-    <div>search</div>
+    <div className="container">
+      <h2 className="title">
+        Result for : <span className="query-text">{query} </span>
+      </h2>
+      <div className="movie-container">
+      {movies.length ===0 && <p> Loading...</p>}
+      {movies.length >0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
   )
 }
